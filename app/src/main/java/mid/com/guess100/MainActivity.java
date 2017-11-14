1cb19321b91594956350e2c7d5403dd9b4b56305
champion0406 <barney0406@gmail.com>
2017-11-14 10:19:48 +0800

    findViews

--- app/src/main/java/mid/com/guess100/MainActivity.java
+++ app/src/main/java/mid/com/guess100/MainActivity.java
@@ -2,6 +2,9 @@ package mid.com.guess100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText secrett = (EditText) findViewById((R.id.secret));
+        EditText info = (EditText) findViewById((R.id.info));
+        EditText ednumber = (EditText) findViewById((R.id.number));
+Button send = (Button) findViewById((R.id.send));
+
+    }
+    public void findviews(View view){
+
     }
 }
