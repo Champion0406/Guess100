26d4104a4f69997a9a76a9f9ce18817babd0800e
champion0406 <barney0406@gmail.com>
2017-11-14 10:46:59 +0800

    Guess

--- app/src/main/java/mid/com/guess100/MainActivity.java
+++ app/src/main/java/mid/com/guess100/MainActivity.java
@@ -1,24 +1,48 @@
 package mid.com.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Random r = new Random();
+    int i = r.nextInt(100) + 1;
+    int min = 1;
+    int max = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText secrett = (EditText) findViewById((R.id.secret));
-        EditText info = (EditText) findViewById((R.id.info));
-        EditText ednumber = (EditText) findViewById((R.id.number));
-Button send = (Button) findViewById((R.id.send));
+
 
     }
-    public void findviews(View view){
 
+    public void test(View view) {
+        EditText ednumber = (EditText) findViewById(R.id.number);
+        int n = Integer.parseInt(ednumber.getText().toString());
+        if (i == n) {
+            new AlertDialog.Builder(this)
+                    .setMessage("答對了")
+                    .setPositiveButton("ok", null)
+                    .show();
+        }
+        if (i > n) {
+            min = n;
+            Toast.makeText(this, min + "to" + max, Toast.LENGTH_LONG).show();
+        }
+        if (i < n) {
+            max = n;
+            Toast.makeText(this,  min + "to" + max, Toast.LENGTH_LONG).show();
+
+        }
     }
 }
